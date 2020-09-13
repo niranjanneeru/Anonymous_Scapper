3e2ffa366d91a027b9c706273751a06220c60da5
Niranjan B <niranjannb7777@gmail.com>
2020-09-13 14:15:27 +0530

    Assignment Pulling

---
+++ assignment.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+from requests_html import HTMLSession
+
+u = "190983"
+p = "Niranjan_2002"
+
+login_cred = {
+    "LoginForm[username]": u,
+    "LoginForm[password]": p,
+}
+session = HTMLSession()
+
+url = "https://tkmce.etlab.in/user/login"
+r = session.post(url, data=login_cred).text
+
+# print(r)
+
+
+# soup = BeautifulSoup('<td><a href="/student/viewassignment/127">LAB 1</a></td>', "html.parser")
+# print(soup.find_all('td')[0].attr)
